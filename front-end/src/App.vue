56a3782751adbb3ebe971e046096330b0abe3458
ogzCode <oguzguc44@gmail.com>
2025-03-20 16:36:44 +0300

    feat: Add main application structure with App.vue, main.ts, and TailwindCSS setup; include utility functions for class management.

---
+++ front-end/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Badge } from '@/components/ui/badge'
+</script>
+
+<template>
+  <div>
+    <Badge>Hello World</Badge>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
