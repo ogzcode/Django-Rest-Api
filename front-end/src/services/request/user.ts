2129181fc258f349a74ab78997e88089446774da
ogzCode <oguzguc44@gmail.com>
2025-03-26 16:38:06 +0300

    feat: Update user-related API URLs to include detailed endpoints for user management and ensure consistent trailing slashes in URL paths.

--- front-end/src/services/request/user.ts
+++ front-end/src/services/request/user.ts
@@ -1,14 +1,18 @@
 import { api } from '@/config/axiosConfig';
 import { user } from '@/services/url';
 
-export const getUserById = async (id: string) => {
-    return await api.get(`${user}/${id}`);
+export const getUserById = async (id: number) => {
+    return await api.get(user.detail(id));
 }
 
-export const updateUserById = async (id: string, data: any) => {
-    return await api.put(`${user}/${id}`, data);
+export const updateUserById = async (id: number, data: any) => {
+    return await api.put(user.update(id), data);
 }
 
-export const deleteUserById = async (id: string) => {
-    return await api.delete(`${user}/${id}`);
+export const deleteUserById = async (id: number) => {
+    return await api.delete(user.delete(id));
+}
+
+export const getUserCompanyById = async (id: number) => {
+    return await api.get(user.company(id));
 }
--- front-end/src/services/url.ts
+++ front-end/src/services/url.ts
@@ -1,9 +1,14 @@
 const baseUrl = import.meta.env.VITE_API_URL;
 
 export const auth = {
-    register: `${baseUrl}/register`,
-    login: `${baseUrl}/login`,
-    refresh: `${baseUrl}/refresh`,
+    register: `${baseUrl}/register/`,
+    login: `${baseUrl}/login/`,
+    refresh: `${baseUrl}/refresh/`,
 }
 
-export const user = `${baseUrl}/user/`
+export const user = {
+    detail: (id: number) => `${baseUrl}/users/${id}`,
+    update: (id: number) => `${baseUrl}/users/update/${id}`,
+    delete: (id: number) => `${baseUrl}/users/delete/${id}`,
+    company: (id: number) => `${baseUrl}/users/company/${id}`,
+}
