be50d75eb21ed92abd95dc9259acfab1fc5f3c7e
ogzCode <oguzguc44@gmail.com>
2025-03-26 16:36:32 +0300

    feat: Refactor API URL configuration to separate user-related views into distinct modules and add update, delete, and company detail endpoints for enhanced user management.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import UserDetailAPIView, CustomTokenObtainPairView, CustomTokenRefreshView, RegisterView
+from .views.auth import CustomTokenObtainPairView, CustomTokenRefreshView, RegisterView
+from .views.users import UserDetailAPIView, UserUpdateAPIView, UserDeleteAPIView, UserCompanyAPIView
 
 urlpatterns = [
-    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
+    path('users/<int:pk>', UserDetailAPIView.as_view(), name='user-detail'),
+    path('users/update/<int:pk>', UserUpdateAPIView.as_view(), name='user-update'),
+    path('users/delete/<int:pk>', UserDeleteAPIView.as_view(), name='user-delete'),
+    path('users/company/<int:pk>', UserCompanyAPIView.as_view(), name='user-company'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
-
-    
