63773daff03888935502d85a9abb33ec179b7549
ogzCode <oguzguc44@gmail.com>
2025-03-26 16:36:24 +0300

    feat: Enhance IsOwnData permission class to validate user access based on query parameters and object attributes for improved data security.

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,17 @@
 from rest_framework.permissions import BasePermission
 
 class IsOwnData(BasePermission):
+    def has_permission(self, request, view):
+        user_id = request.query_params.get('id')
+        if not user_id or int(user_id) != request.user.id:
+            return False
+        return True
+    
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        if hasattr(obj, 'user'):
+            return obj.user == request.user
+        elif hasattr(obj, 'id'):
+            return obj.id == request.user.id
+        return False
     
     
