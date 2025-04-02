c5386001622b498d000f670d2cf6162ff72f958d
ogzCode <oguzguc44@gmail.com>
2025-04-02 11:44:11 +0300

    feat: Update IsOwnData permission class to enhance user authentication checks and object permission handling for staff users.

--- api/permissions.py
+++ api/permissions.py
@@ -2,16 +2,18 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnData(BasePermission):
     def has_permission(self, request, view):
-        user_id = request.query_params.get('id')
-        if not user_id or int(user_id) != request.user.id:
-            return False
-        return True
+        return request.user and request.user.is_authenticated
     
     def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+            
         if hasattr(obj, 'user'):
             return obj.user == request.user
-        elif hasattr(obj, 'id'):
+            
+        elif hasattr(obj, 'id') and hasattr(request.user, 'id'):
             return obj.id == request.user.id
+            
         return False
     
     
